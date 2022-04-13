c546e0cd75455fe835d071bb96a6fcc44503b266
oaat9309 <kite1993@naver.com>
2022-04-13 10:55:05 +0900

    Project Detail Components

--- pages/Movie_App.tsx
+++ pages/Movie_App.tsx
@@ -1,11 +1,23 @@
 import type { NextPage } from "next";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
+import projectImage from "../public/movieApp.jpeg";
 
 const MovieApp: NextPage = () => {
-  return <div>Movie Searching App</div>;
+  return (
+    <div>
+      <Image src={projectImage} alt="" />
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </div>
+      <div className="mt-4">
+        <Link href="/">
+          <a className="font-bold underline hover:text-indigo-600">Back</a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default MovieApp;
